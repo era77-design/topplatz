5bc0c2c34139be56408473685662948a4e139270
era77-design <egorenko.r@gmail.com>
2026-06-15 00:56:15 +0200

    revert to next-on-pages: home/categories/category as edge dynamic pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.output.globalObject = 'globalThis'
-    }
-    return config
-  },
-};
+const nextConfig = {};
 export default nextConfig;
