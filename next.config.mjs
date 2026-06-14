dcaa26313df5ab1fd67fa6101a9224b2b2a79a1e
era77-design <egorenko.r@gmail.com>
2026-06-15 00:44:36 +0200

    fix self is not defined: webpack globalObject=globalThis for export

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.output.globalObject = 'globalThis'
+    }
+    return config
+  },
 };
 export default nextConfig;
